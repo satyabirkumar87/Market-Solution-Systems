aa652da494ddf51250d090f2fedbe6f855000ac1
Shakir <shakirali.sacube@gmail.com>
2023-03-29 19:42:58 +0530

    Completed Products Purchase Module

--- Market Solutions System/Market Solutions System/ProductPurchase.vb
+++ Market Solutions System/Market Solutions System/ProductPurchase.vb
@@ -2,28 +2,40 @@
 
 
 Public Class ProductPurchase
-    Private conn As SQLiteConnection = MainForm.conn
+    Private conn As SQLiteConnection = Login.conn
+    Dim ProductList As New Dictionary(Of String, Integer)
     Private Sub Purchase_Click(sender As Object, e As EventArgs) Handles PurchaseBtn.Click
-        Dim ProdName As String = ProdNameCB.Text
-        Dim price As Integer = PriceTB.Text
-        Dim quantity As Integer = QtyTB.Text
-        MessageBox.Show(ProdName)
-        MessageBox.Show(price)
-        MessageBox.Show(quantity)
-
-        MessageBox.Show("purchase sucessful")
+        If ProdNameCB.Text = "" Or QtyTB.Text = "" Then
+            MessageBox.Show("Select A Product And Enter Quantity First")
+        Else
+            totalTB.Text = CInt(PriceTB.Text) * CInt(QtyTB.Text)
+            conn.Open()
+            Dim sqlQuery As String = $"UPDATE product SET ProdQty = ProdQty - {QtyTB.Text} WHERE Name='{ProdNameCB.Text}'"
+            Dim cmd As New SQLiteCommand(sqlQuery, conn)
+            Dim rowsAffected = cmd.ExecuteNonQuery()
+            If rowsAffected = 1 Then
+                MessageBox.Show("Purchase Sucessful")
+            End If
+            conn.Close()
+        End If
 
 
     End Sub
 
     Private Sub ProductPurchase_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ' conn.Open()
-        ' Dim cmd As New SQLiteCommand("select * from product", conn)
-        'Dim reader = cmd.ExecuteReader()
-        'While reader.Read()
-        'ProdNameCB.Items.Add(reader("Name"))
-        'End While
-        'conn.Close()
+        ' Add items to dictionary
+        conn.Open()
+        Dim cmd As New SQLiteCommand("select * from product", conn)
+        Dim reader = cmd.ExecuteReader()
+        While reader.Read()
+            ProductList.Add(reader("Name"), reader("Price"))
+        End While
+        conn.Close()
+
+        ' Loop through each key in the dictionary and add it to the dropdown
+        For Each prodName As String In ProductList.Keys
+            ProdNameCB.Items.Add(prodName)
+        Next
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
@@ -32,31 +44,6 @@ Public Class ProductPurchase
     End Sub
 
     Private Sub ProdNameCB_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ProdNameCB.SelectedIndexChanged
-        If (ProdNameCB.SelectedItem = "coco cola") Then
-            PriceTB.Clear()
-            PriceTB.Text = "20"
-        ElseIf (ProdNameCB.SelectedItem = "kurkure") Then
-            PriceTB.Clear()
-            PriceTB.Text = "50"
-        ElseIf (ProdNameCB.SelectedItem = "kitkat") Then
-            PriceTB.Clear()
-            PriceTB.Text = "35"
-        ElseIf (ProdNameCB.SelectedItem = "jam") Then
-            PriceTB.Clear()
-            PriceTB.Text = "95"
-        ElseIf (ProdNameCB.SelectedItem = "Bourborn") Then
-            PriceTB.Clear()
-            PriceTB.Text = "45"
-        ElseIf (ProdNameCB.SelectedItem = "Lays") Then
-            PriceTB.Clear()
-            PriceTB.Text = "39"
-        ElseIf (ProdNameCB.SelectedItem = "Top") Then
-            PriceTB.Clear()
-            PriceTB.Text = "100"
-        ElseIf (ProdNameCB.SelectedItem = "Amul") Then
-            PriceTB.Clear()
-            PriceTB.Text = "60"
-
-        End If
+        PriceTB.Text = ProductList.Item(ProdNameCB.Text)
     End Sub
 End Class
